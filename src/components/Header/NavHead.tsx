e16242e75815d181db0165ddb0e41aa2768f610e
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-04-22 02:49:18 +0100

    Modify navbar in header

--- src/components/Header/NavHead.tsx
+++ src/components/Header/NavHead.tsx
@@ -3,8 +3,8 @@ import { Navbar, NavbarBrand, NavItem } from "reactstrap";
 
 const NavHead = () => {
   return (
-    <Navbar color="dark" expand="xl" id="navhead">
-        <NavbarBrand href="/" className="linkText"><img src="./fjd.png" alt="FJD logo" id="fjd"/></NavbarBrand>
+    <Navbar color="dark" id="navhead">
+        <NavbarBrand href="/" id="logo"><img src="./fjd.png" alt="FJD logo" id="fjd"/></NavbarBrand>
         <NavItem>
             <NavLink to='/misdemeanour' className="linkText">Misdemeanours</NavLink>
         </NavItem>

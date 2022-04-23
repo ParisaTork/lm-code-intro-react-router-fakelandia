ef12573ed5e032bbe056a1dcd099c76c404c8edf
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-04-23 22:04:52 +0100

    Open linkTextFooter elems in new tab

--- src/components/Footer/NavFoot.tsx
+++ src/components/Footer/NavFoot.tsx
@@ -3,8 +3,8 @@ import { Navbar, NavbarBrand } from "reactstrap";
 const NavFoot = () => {
   return (
     <Navbar color="dark" expand="xl" id="navfoot">
-        <NavbarBrand className="linkTextFooter" href="https://translate.google.co.uk/?sl=auto&tl=en&text=in%20fakelandia%20confidimus&op=translate">In Fakelandia confidimus</NavbarBrand>
-        <NavbarBrand className="linkTextFooter" href="https://github.com/ParisaTork">© Parisa Tork</NavbarBrand>
+        <NavbarBrand className="linkTextFooter" href="https://translate.google.co.uk/?sl=auto&tl=en&text=in%20fakelandia%20confidimus&op=translate" target="_blank">In Fakelandia confidimus</NavbarBrand>
+        <NavbarBrand className="linkTextFooter" href="https://github.com/ParisaTork" target="_blank">© Parisa Tork</NavbarBrand>
     </Navbar>
   )
 }

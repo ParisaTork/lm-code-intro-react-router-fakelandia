fc3ce05e8d9087a80851b10a32401563364216af
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-04-24 00:11:28 +0100

    Improve footer cross-platform compatibility

--- src/components/Footer/NavFoot.tsx
+++ src/components/Footer/NavFoot.tsx
@@ -1,10 +1,12 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 const NavFoot = () => {
   return (
-    <Navbar color="dark" expand="xl" id="navfoot">
-        <NavbarBrand className="linkTextFooter" href="https://translate.google.co.uk/?sl=auto&tl=en&text=in%20fakelandia%20confidimus&op=translate" target="_blank">In Fakelandia confidimus</NavbarBrand>
-        <NavbarBrand className="linkTextFooter" href="https://github.com/ParisaTork" target="_blank">© Parisa Tork</NavbarBrand>
+    <Navbar color="dark" fixed="bottom" expand="xl" id="navfoot">
+        <Container>
+          <Nav.Link className="linkTextFooter" href="https://translate.google.co.uk/?sl=auto&tl=en&text=in%20fakelandia%20confidimus&op=translate" target="_blank">In Fakelandia confidimus</Nav.Link>
+          <Nav.Link className="linkTextFooter" href="https://github.com/ParisaTork" target="_blank">© Parisa Tork</Nav.Link>
+        </Container>
     </Navbar>
   )
 }

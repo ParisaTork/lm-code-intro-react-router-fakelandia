f3b207823b827fd7c14a07c43aa0a9ee66dc3f5a
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-04-24 18:38:40 +0100

    Activity 2: Highlight NavLink

--- src/components/Header/NavSubHead.tsx
+++ src/components/Header/NavSubHead.tsx
@@ -8,12 +8,20 @@ const NavSubHead = () => {
         <Row>
         <Col className="col-6">
         <NavItem className="nav-item text-center">
-            <NavLink className="linkTextNavSubHead" to='/misdemeanour'>Misdemeanours</NavLink>
+            <NavLink 
+                    className="linkTextNavSubHead" 
+                    to='/misdemeanour'
+                    style={({ isActive }) => ({ color: isActive ? '#F93B4C' : '#1C247C' })}
+                    >Misdemeanours</NavLink>
         </NavItem>
         </Col>
         <Col className="col-6">
         <NavItem className="nav-item text-center">
-            <NavLink className="linkTextNavSubHead" to='/confession'>Confess to Us</NavLink>
+            <NavLink 
+                    className="linkTextNavSubHead"  
+                    to='/confession' 
+                    style={({ isActive }) => ({ color: isActive ? '#F93B4C' : '#1C247C' })}
+                    >Confess to Us</NavLink>
         </NavItem>
         </Col>
         </Row>

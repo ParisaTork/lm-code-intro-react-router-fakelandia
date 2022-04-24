cdffafd616ca3c3bdb30f35571baf85ee0225220
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-04-24 22:15:16 +0100

    Fix logo bug

--- src/components/Header/NavHead.tsx
+++ src/components/Header/NavHead.tsx
@@ -1,9 +1,10 @@
 import { Navbar, NavbarBrand } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const NavHead = () => {
   return (
     <Navbar color="dark" id="navhead">
-        <NavbarBrand href="/" id="logo"><img src="./fjd.png" alt="FJD logo" id="fjd"/></NavbarBrand>
+        <NavbarBrand tag={Link} to="/" id="logo"><img src="./fjd.png" alt="FJD logo" id="fjd"/></NavbarBrand>
     </Navbar>
   )
 }
